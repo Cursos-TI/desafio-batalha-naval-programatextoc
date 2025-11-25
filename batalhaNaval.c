ef068597566979aebc175167a9fd4f76eece89b8
programatextoc <cleberbezerra71@gmail.com>
2025-11-25 18:44:21 +0000

    Novato criado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,6 +6,15 @@ printf("Tabuleiro batalha naval:\n");
 
 int Tabuleiro [10][10]={0};
 char Colunas[10]={'A','B','C','D','E','F','G','H','I','J'};
+
+//Posição do navio horizontal
+for(j=3;j<=5;j++){
+Tabuleiro[6][j]=3;}
+
+//Posição do Navio na vertical
+for(i=5;i<=7;i++){
+Tabuleiro[i][9]=3;}
+
 printf("  ");
 for(int j=0; j<10; j++){
 printf("%c ", Colunas[j]);}
@@ -16,7 +25,6 @@ printf("%2d ", i+1);
 
 for(int j=1 ;j<10 ;j++){
 printf("%d ", Tabuleiro[i][j]);}
-
 printf("\n");}
 
 return 0;}
