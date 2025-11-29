eefa2472c5199184a13a017d31061c50f3d74de0
programatextoc <cleberbezerra71@gmail.com>
2025-11-29 00:25:11 +0000

    Avançado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -41,6 +41,43 @@ for(Coluna=0;Coluna<10;Coluna++){
 printf("%d ",Tabuleiro[Linha][Coluna]);}
 printf("\n");}
 
+//Octaedro
+int octaedro=4;
+int LinhaCentral=3;
+int Colunacentral=7;
+int Colunahorizontal=7;
+
+for(i=0;i<10;i++){
+for(j=0;j<10;j++){
+
+int DistanciaVertical;
+if(i>LinhaCentral){
+DistanciaVertical=i-LinhaCentral;}
+if(i<=LinhaCentral){
+DistanciaVertical=LinhaCentral-i;}
+
+int DistanciaHorizontal;
+if(j>Colunacentral){
+DistanciaHorizontal=j-Colunacentral;}
+if(j<=Colunacentral){
+DistanciaHorizontal=Colunacentral-j;}
+
+if(DistanciaVertical+DistanciaHorizontal<=2){
+Tabuleiro[i][j]=octaedro;}}}
+
+//octaedro impressão
+printf("\n***Octaedro***\n");
+printf("   ");
+for(j=0;j<10;j++){
+printf("%c ", Colunas[j]);}
+printf("\n");
+
+for(i=0;i<10;i++){
+printf("%2d ", i+1);
+for(j=0;j<10;j++){
+printf("%d ",Tabuleiro[i][j]);}
+printf("\n");}
+
 
 //Posição do navio diagonal
 for(i=7;i<=9;i++){
